72b19cf22f26abaaf92d37aad3b2bf1c92514377
Kadirov Muhammadaziz <126769438+KadirovKmck@users.noreply.github.com>
2025-01-18 10:51:13 +0600

    start

---
+++ android/app/src/main/kotlin/com/example/ai_lawyer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ai_lawyer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:ai_lawyer/src/app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
