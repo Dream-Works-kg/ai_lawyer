a20d96e49c293b77144d7b2c7688307c96c1f39d
LWVX <lwvxab@gmail.com>
2025-01-20 13:22:06 +0300

    its done

---
+++ lib/src/presentations/auth/view/sign_up/sign_sup.dart
@@ -0,0 +1,16 @@
+import 'package:ai_lawyer/src/core/components/components.dart';
+import 'package:flutter/material.dart';
+
+class SignSup extends StatelessWidget {
+  const SignSup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
---
+++ lib/src/presentations/welcome/welcome.dart
@@ -0,0 +1,16 @@
+import 'package:ai_lawyer/src/core/components/components.dart';
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
+ 
